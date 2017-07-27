1a344b57cc169c81cb8b48156eb41e157126ff0b
Marco <marco@gmx.com>
2017-07-27 22:35:02 +0200

    Add Heater modul

---
+++ inc/Heater.h
@@ -0,0 +1,14 @@
+/*
+ * Heater.h
+ *
+ *  Created on: 27.07.2017
+ *      Author: Marco
+ */
+
+#ifndef INC_HEATER_H_
+#define INC_HEATER_H_
+
+extern void Heater_Init(void);
+
+
+#endif /* INC_HEATER_H_ */
---
+++ src/Heater.c
@@ -0,0 +1,17 @@
+/*
+ * Heater.c
+ *
+ *  Created on: 27.07.2017
+ *      Author: Marco
+ */
+
+#include "stm32f10x.h"
+
+#include "Heater.h"
+
+void Heater_Init(void)
+{
+	//TIM_DeInit()
+
+
+}
