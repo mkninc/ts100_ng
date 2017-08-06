d34c5337b326c47ef27b647231f2489cec9253d8
Marco <marco@gmx.com>
2017-08-06 17:24:48 +0200

    Function Graph_DrawPixel(), allow deleting pixel

--- inc/graphbuffer.h
+++ inc/graphbuffer.h
@@ -21,4 +21,6 @@ extern void Graph_DrawPixel(u32 const x, u32 const y, u32 const value);
 
 extern void Graph_DrawHorizontalBar(u32 const x, u32 const y, u32 const length);
 
+extern void Graph_DrawArea(u32 const x, u32 const y, u32 const width, u32 const height, u8 const * data);
+
 #endif /* INC_GRAPHBUFFER_H_ */
--- src/graphbuffer.c
+++ src/graphbuffer.c
@@ -11,38 +11,52 @@
 
 static u8 gr_buffer[2][96];
 
-void Graphic_Init(void)
-{
+//-----------------------------------------------------------------------------
+void Graphic_Init(void) {
 	Graph_Clear();
 }
+//-----------------------------------------------------------------------------
 
-void Graph_Update(void)
-{
-Oled_DrawArea(0, 0, 96, 8, &gr_buffer[0][0]);
-Oled_DrawArea(0, 8, 96, 8, &gr_buffer[1][0]);
+//-----------------------------------------------------------------------------
+void Graph_Update(void) {
+	Oled_DrawArea(0, 0, 96, 8, &gr_buffer[0][0]);
+	Oled_DrawArea(0, 8, 96, 8, &gr_buffer[1][0]);
 }
+//-----------------------------------------------------------------------------
 
-void Graph_Clear(void)
-{
+//-----------------------------------------------------------------------------
+void Graph_Clear(void) {
 	memset(gr_buffer, 0, sizeof(gr_buffer));
 }
+//-----------------------------------------------------------------------------
 
-void Graph_DrawHorizontalBar(u32 const x, u32 const y, u32 const length)
-{
-	for(u32 i = x; i < x + length; i++)
-	{
+//-----------------------------------------------------------------------------
+void Graph_DrawHorizontalBar(u32 const x, u32 const y, u32 const length) {
+	for (u32 i = x; i < x + length; i++) {
 		Graph_DrawPixel(i, y, 0xFF);
 	}
 }
+//-----------------------------------------------------------------------------
 
-
-void Graph_DrawPixel(u32 const x, u32 const y, u32 const value)
-{
+//-----------------------------------------------------------------------------
+void Graph_DrawPixel(u32 const x, u32 const y, u32 const value) {
 	u32 row;
 	u8 pixel;
 
 	row = (y / 8);
 	pixel = 1 << (y % 8);
 
-	gr_buffer[row][x] |= pixel;
+	if (value) {
+		gr_buffer[row][x] |= pixel;
+	} else {
+		gr_buffer[row][x] &= ~pixel;
+	}
 }
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+void Graph_DrawArea(u32 const x, u32 const y, u32 const width, u32 const height,
+		u8 const * data) {
+
+}
+//-----------------------------------------------------------------------------
